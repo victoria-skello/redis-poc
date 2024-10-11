c476e41a9c9dd6af8e031997eebabab877488708
victoria-skello <victoria.doe@skello.io>
2024-10-11 16:29:09 +0200

    feat(redis-poc): add compression

--- src/middleware/redis.ts
+++ src/middleware/redis.ts
@@ -1,16 +1,17 @@
 import hash from 'object-hash'
 import { REDIS_URL } from '../config/lib/dotenv'
 import { createClient } from 'redis'
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express'
+import zlib from 'zlib'
 
-let redisClient;
+let redisClient
 
 export const initRedisClient = async () => {
-    redisClient = createClient({ url: REDIS_URL }).on("error", (e) => {
+    redisClient = createClient({ url: REDIS_URL }).on('error', (e) => {
         console.error(`Failed to create the Redis client with error:`, e)
-    });
+    })
     try {
-        await redisClient.connect();
+        await redisClient.connect()
         console.log('Connected to Redis successfully')
     } catch (e) {
         console.error('Failed to connect to Redis with error:', e)
@@ -22,45 +23,53 @@ const requestToKey = (req: Request) => {
         param: req.params,
         body: req.body,
     }
-    return `${req.path}@${hash.sha1(toHash)}`;
+    return `${req.path}@${hash.sha1(toHash)}`
 }
 
 const isRedisReady = () => {
     return redisClient.isOpen
 }
 
-const write = async (key: unknown, value: string, options: unknown) => {     
+const write = async (key: unknown, value: string, options: unknown, compressed: boolean) => {     
     if (isRedisReady()) {
         try {
-            await redisClient.set(key, value, options);
+            const dataToCache = compressed ? zlib.deflateSync(value).toString('base64') : value
+            await redisClient.set(key, dataToCache, options)
         } catch(e) {
             console.error(`Failed to cache data for key=${key}`, e)
         }
     }
 }
 
-const read = async (key: string) => {  
+const read = async (key: string, compressed: boolean) => {  
     if (isRedisReady()) {
-        return await redisClient.get(key)
+        try {
+            const value = await redisClient.get(key)
+            if (value) {
+                return compressed ? zlib.inflateSync(Buffer.from(value, 'base64')).toString() : value
+            }
+        } catch(e) {
+            console.error(`Failed to read data for key=${key}`, e)
+        }
     }  
 }
 
-export const redisCachingMiddleware = (options = { EX: 10800 }) => {
+export const redisCachingMiddleware = (options = { EX: 10800 }, compression = true) => {
     return async (req: Request, res: Response, next: NextFunction) => {
       if (isRedisReady()) {
         const key = requestToKey(req)
-        const cachedValue = await read(key)
+        const cachedValue = await read(key, compression)
         if (cachedValue) {
           return res.json(JSON.parse(cachedValue))
         } else {
           const oldSend = res.send
-          res.send = function (data) {
+          res.send = (data) => {
             res.send = oldSend
-            if (res.statusCode.toString().startsWith("2")) {
-              write(key, data, options).then()
+            if (res.statusCode.toString().startsWith('2')) {
+              write(key, data, options, compression).then()
             }
             return res.send(data)
-          };
+          }
           next()
         }
       } else {
